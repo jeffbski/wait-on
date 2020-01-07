0840c8696f1d5ae946ca5c143cb8049d64deb6b1
Jeff Barczewski <jeff.barczewski@gmail.com>
2020-01-07 08:48:54 -0600

    formatting and additional test

--- test/validation.mocha.js
+++ test/validation.mocha.js
@@ -12,35 +12,38 @@ function execCLI(args, options) {
   return childProcess.exec('../bin/wait-on', args, options);
 }
 
-
-describe('validation', function () {
-
-  describe('API', function () {
-    it('should return error when no resources are provided', function (done) {
+describe('validation', function() {
+  describe('API', function() {
+    it('should callback with error when resources property is omitted', function(done) {
       var opts = {};
-      waitOn(opts, function (err) {
+      waitOn(opts, function(err) {
         expect(err).toExist();
         done();
       });
     });
 
-    it('should return error when opts is null', function (done) {
-      waitOn(null, function (err) {
-        expect(err).toExist();
+    it('should callback with error when no resources are provided', function(done) {
+      const opts = { resources: [] };
+      waitOn(opts, function(err) {
+        expect(err.toString()).toInclude('"resources" does not contain 1 required value(s)');
         done();
       });
     });
 
-  });
-
-  describe('CLI', function () {
-    it('should exit with non-zero error code when no resources provided', function (done) {
-      execCLI([])
-        .on('exit', function (code) {
-          expect(code).toNotBe(0);
-          done();
-        });
+    it('should return error when opts is null', function(done) {
+      waitOn(null, function(err) {
+        expect(err).toExist();
+        done();
+      });
     });
   });
 
+  describe('CLI', function() {
+    it('should exit with non-zero error code when no resources provided', function(done) {
+      execCLI([]).on('exit', function(code) {
+        expect(code).toNotBe(0);
+        done();
+      });
+    });
+  });
 });
